c6bd0c9e12f1d0902ffb7c6eeb8c318c785be0f8
cyrus-trixie <125303529+cyrus-trixie@users.noreply.github.com>
2025-07-05 14:11:47 +0300

    up to date

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel"],
+    presets: [
+      ['babel-preset-expo', {
+        unstable_transformImportMeta: true, // Add this line here
+      }],
+    ],
+    plugins: ["nativewind/babel"], // Keep your existing plugins
   };
 };
